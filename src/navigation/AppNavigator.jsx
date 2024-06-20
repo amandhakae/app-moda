2ae697f82a6185b416a83b50301ec4dcd65226be
Your name <you@example.com>
2024-06-20 14:21:54 -0300

    location-expo

--- src/navigation/AppNavigator.jsx
+++ src/navigation/AppNavigator.jsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "../screens/LoginScreen";
 import NewsScreen from "../screens/NewsScreen";
+import LocationScreen from "../screens/LocationScreen";
 
 const Stack = createStackNavigator();
 export default function AppNavigator() {
@@ -10,7 +11,9 @@ export default function AppNavigator() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Location" component={LocationScreen} />
         <Stack.Screen name="News" component={NewsScreen} />
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
